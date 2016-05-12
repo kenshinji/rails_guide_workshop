25f579e581160c32871fda89f2e08d649b26b3d4
kenshinji <gundam0083ster@gmail.com>
2016-05-12 05:08:30 +0000

    finished 5.11 Updating Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,9 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new
 	end
+	def edit
+		@article = Article.find(params[:id])
+	end
 
 	def create
 		@article = Article.new(article_params)
@@ -13,6 +16,16 @@ class ArticlesController < ApplicationController
 		        redirect_to @article
 		else
 			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
 		end
 	end
 
